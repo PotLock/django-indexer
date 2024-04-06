e9cc1327f2faca4cd6539c6815239a623983822e
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-04-06 11:54:05 -0400

    add black formatting

--- indexer_app/models.py
+++ indexer_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Account(models.Model):
+    id = models.CharField(
+        _("address"),
+        primary_key=True,
+        max_length=64,
+        db_index=True,
+        validators=[],
+        help_text=_("On-chain account address."),
+    )
+    total_donations_received_usd = models.DecimalField(
+        _("total donations received in USD"),
+        max_digits=20,
+        decimal_places=2,
+        default=0,
+        help_text=_("Total donations received in USD."),
+    )
