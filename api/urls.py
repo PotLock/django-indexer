d7e91e4caee6865c292f8c55f399dccf6f638ca9
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-04-12 10:44:31 -0400

    add api app

---
+++ api/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    # Add API URLs here
+]
--- base/settings.py
+++ base/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "celery",
+    "api",
     "accounts",
     "activities",
     "donations",
