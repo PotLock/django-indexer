4df673af8bfb5d8bdc8e2a5927370fadec29da76
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-04-16 11:32:12 -0400

    add django-cachalot for ORM-level caching

--- base/settings.py
+++ base/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+from distutils.util import strtobool
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,8 +32,8 @@ ALLOWED_HOSTS = []
 # Env vars
 AWS_ACCESS_KEY_ID = os.environ.get("PL_AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("PL_AWS_SECRET_ACCESS_KEY")
-REDIS_HOST = os.environ.get("PL_REDIS_HOST", "localhost")
-REDIS_PORT = os.environ.get("PL_REDIS_PORT", 6379)
+CACHALOT_ENABLED = strtobool(os.environ.get("PL_CACHALOT_ENABLED", "True"))
+CACHALOT_TIMEOUT = os.environ.get("PL_CACHALOT_TIMEOUT")
 POSTGRES_DB = os.environ.get("PL_POSTGRES_DB")
 POSTGRES_HOST = os.environ.get("PL_POSTGRES_HOST", None)
 POSTGRES_PASS = os.environ.get("PL_POSTGRES_PASS", None)
@@ -40,6 +41,8 @@ POSTGRES_PORT = os.environ.get("PL_POSTGRES_PORT", None)
 POSTGRES_READONLY_PASS = os.environ.get("PL_POSTGRES_READONLY_PASS", None)
 POSTGRES_READONLY_USER = os.environ.get("PL_POSTGRES_READONLY_USER", None)
 POSTGRES_USER = os.environ.get("PL_POSTGRES_USER", None)
+REDIS_HOST = os.environ.get("PL_REDIS_HOST", "localhost")
+REDIS_PORT = os.environ.get("PL_REDIS_PORT", 6379)
 
 BLOCK_SAVE_HEIGHT = os.environ.get("BLOCK_SAVE_HEIGHT")
 
@@ -53,6 +56,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "cachalot",
     "celery",
     "api",
     "accounts",
@@ -119,6 +123,13 @@ CACHES = {
         "TIMEOUT": 300,  # 5 minutes
     }
 }
+
+if CACHALOT_TIMEOUT:
+    CACHALOT_TIMEOUT = int(CACHALOT_TIMEOUT)
+else:
+    CACHALOT_TIMEOUT = 60 * 5  # 5 minutes
+
+CACHALOT_UNCACHABLE_TABLES = frozenset(("django_migrations",))
 
 
 ###############################################################################
