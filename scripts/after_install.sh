5eff58c08ed4a02306a3030cdd68386fae84cc78
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-04-30 10:22:22 -0400

    update after_install script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
-echo 'run after_install.sh' >> /home/ec2-user/myrepo/deploy.log
+# Log output to a specific file
+LOG_FILE="/home/ec2-user/django-indexer/logs/deploy.log"
 
-cd /home/ec2-user/django-indexer
+echo 'Running after_install.sh' >> "$LOG_FILE"
 
-echo 'poetry install' >> /home/ec2-user/myrepo/deploy.log 
+# Define the project directory
+PROJECT_DIR="/home/ec2-user/django-indexer"
 
-poetry shell
+# Navigate to the project directory
+cd "$PROJECT_DIR"
 
-poetry install >> /home/ec2-user/myrepo/deploy.log
+# Source the specific poetry virtual environment
+source "/home/ec2-user/.cache/pypoetry/virtualenvs/django-indexer-Y-SQFfhb-py3.11/bin/activate"
+
+# Check if there are pending migrations
+if python manage.py showmigrations | grep '\[ \]'; then
+    echo 'Migrations found, stopping services...' >> "$LOG_FILE"
+    sudo systemctl stop gunicorn.service celery.service
+
+    echo 'Applying migrations...' >> "$LOG_FILE"
+    python manage.py migrate >> "$LOG_FILE"
+
+    echo 'Starting services...' >> "$LOG_FILE"
+    sudo systemctl start gunicorn.service celery.service
+else
+    echo 'No migrations found. Running collectstatic and restarting services...' >> "$LOG_FILE"
+    python manage.py collectstatic --noinput >> "$LOG_FILE"
+    sudo systemctl restart gunicorn.service celery.service
+fi
+
+echo 'after_install.sh completed' >> "$LOG_FILE"
