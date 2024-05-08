d29667430063ce1c47b04fd99871fb45479ed882
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-05-08 08:43:25 -0400

    remove manual start block setting

--- indexer_app/tasks.py
+++ indexer_app/tasks.py
@@ -57,8 +57,8 @@ def listen_to_near_events():
 
     try:
         # Update below with desired network & block height
-        # start_block = get_block_height("current_block_height")
-        start_block = 105_854_538  # manually setting for reindexing TODO: remove this
+        start_block = get_block_height("current_block_height")
+        # start_block = 105_854_538  # manually setting for reindexing TODO: remove this
         logger.info(f"what's the start block, pray tell? {start_block-1}")
         loop.run_until_complete(indexer("mainnet", start_block - 1, None))
     finally:
