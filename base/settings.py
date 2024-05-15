fd0e2d2942e3e8cbdffefc0ad1ac666a930d33b4
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-05-14 20:59:09 -0400

    add cors headers & localhost support

--- base/settings.py
+++ base/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     # "cachalot",
     "celery",
     "api",
@@ -88,6 +89,7 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -116,6 +118,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "base.wsgi.application"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # REDIS / CACHE CONFIGS
 
--- indexer_app/tasks.py
+++ indexer_app/tasks.py
@@ -58,7 +58,7 @@ def listen_to_near_events():
     try:
         # Update below with desired network & block height
         # start_block = get_block_height("current_block_height")
-        start_block = 106435734  # manually setting for reindexing TODO: remove this
+        start_block = 111854642
         logger.info(f"what's the start block, pray tell? {start_block-1}")
         loop.run_until_complete(indexer("mainnet", start_block - 1, None))
     finally:
