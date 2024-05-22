6307a8905c8188c774d520752cfe92e80afafc71
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-05-22 13:11:24 -0400

    update start block to 113155075

--- indexer_app/tasks.py
+++ indexer_app/tasks.py
@@ -59,7 +59,7 @@ def listen_to_near_events():
     try:
         # Update below with desired network & block height
         # start_block = get_block_height("current_block_height")
-        start_block = 113066329
+        start_block = 113155075
         logger.info(f"what's the start block, pray tell? {start_block-1}")
         loop.run_until_complete(indexer("mainnet", start_block - 1, None))
     except WorkerLostError:
