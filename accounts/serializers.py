1ebd5778325d3f2e4e3a85f23730b784b8daa63f
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-06-06 15:28:02 -0400

    update SIMPLE_ACCOUNT_EXAMPLE in OpenAPI schema to include near_social_profile_data

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -23,4 +23,26 @@ SIMPLE_ACCOUNT_EXAMPLE = {
     "total_donations_out_usd": "1234.56",
     "total_matching_pool_allocations_usd": "800.01",
     "donors_count": 321,
+    "near_social_profile_data": {
+        "name": "Illia",
+        "image": {
+            "nft": {
+                "media": "https://ipfs.nftstorage.link/ipfs/bafybeie6mpnk6iya3wvwtxtogzmzpprw5734dydoeujo5esqqxmmirug6y",
+                "baseUri": "https://arweave.net/q8IenkSo5aogF-bIphzedrom24OFYGECZYUs9gEfM0A",
+                "tokenId": "8120",
+                "contractId": "citizen.bodega-lab.near",
+            }
+        },
+        "linktree": {
+            "github": "ilblackdragon",
+            "twitter": "ilblackdragon",
+            "website": "near.org",
+            "telegram": "",
+        },
+        "description": "Bringing 1B users to web3",
+        "horizon_tnc": "true",
+        "backgroundImage": {
+            "ipfs_cid": "bafkreiemktmsdhpdoomwlvfi2ztm7c5sdqdmb2z5mg4bjssoqkz7wunaoi"
+        },
+    },
 }
