56aff6c1ffa483d98e5980cecdbdae934b9d2c14
Boluwatife Popoola <olaoyejnr@gmail.com>
2024-07-10 17:20:22 +0100

    add blacklist entry model

--- indexer_app/handler.py
+++ indexer_app/handler.py
@@ -8,7 +8,7 @@ from near_lake_framework import near_primitives
 
 from base.utils import convert_ns_to_utc
 from pots.utils import match_pot_factory_pattern, match_pot_subaccount_pattern
-
+from nadabot.utils import match_nadabot_registry_pattern
 from .logging import logger
 from .utils import (  # handle_batch_donations,
     handle_add_stamp,
@@ -16,7 +16,7 @@ from .utils import (  # handle_batch_donations,
     handle_list_admin_removal,
     handle_list_registration_update,
     handle_list_upvote,
-    handle_nadabot_admin_add,
+    handle_add_nadabot_admin,
     handle_nadabot_registry,
     handle_new_donation,
     handle_new_group,
@@ -33,6 +33,8 @@ from .utils import (  # handle_batch_donations,
     handle_social_profile_update,
     handle_transfer_payout,
     handle_update_default_human_threshold,
+    handle_registry_blacklist_action,
+    handle_registry_unblacklist_action
     handle_pot_config_update
 )
 
@@ -40,6 +42,7 @@ from .utils import (  # handle_batch_donations,
 async def handle_streamer_message(streamer_message: near_primitives.StreamerMessage):
     block_timestamp = streamer_message.block.header.timestamp
     block_height = streamer_message.block.header.height
+    now_datetime = datetime.fromtimestamp(block_timestamp / 1000000000)
     await cache.aset(
         "block_height", block_height
     )  # TODO: add custom timeout if it should be valid for longer than default (5 minutes)
@@ -61,7 +64,7 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
                 not in receipt_execution_outcome.execution_outcome.outcome.status
             ):
                 continue
-            receiver_id = receipt_execution_outcome.receipt.receiver_id
+            receiver_id = receipt_execution_outcome.receiver_id
             if (
                 receiver_id != settings.NEAR_SOCIAL_CONTRACT_ADDRESS
                 and not receiver_id.endswith((settings.POTLOCK_TLA, settings.NADABOT_TLA))
@@ -70,11 +73,11 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
             # 1. HANDLE LOGS
             log_data = []
             receipt = receipt_execution_outcome.receipt
+            signer_id = receipt.receipt["Action"]["signer_id"]
 
             for log_index, log in enumerate(
                 receipt_execution_outcome.execution_outcome.outcome.logs, start=1
             ):
-                logger.info(f"didier drigba..: {log}")
                 if not log.startswith("EVENT_JSON:"):
                     continue
                 try:
@@ -85,13 +88,17 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
                     print("parsa parsa...", parsed_log)
                     event_name = parsed_log.get("event")
                     if event_name == "add_or_update_provider":
-                        await handle_new_provider(parsed_log.get("data")[0], receipt.receiver_id, receipt.receipt["Action"]["signer_id"])
+                        await handle_new_provider(parsed_log.get("data")[0], receiver_id, signer_id)
                     elif event_name == "add_stamp":
-                        await handle_add_stamp(parsed_log.get("data")[0], receipt.receiver_id, receipt.receipt["Action"]["signer_id"])
+                        await handle_add_stamp(parsed_log.get("data")[0], receiver_id, signer_id)
                     elif event_name == "update_default_human_threshold":
-                        await handle_update_default_human_threshold(parsed_log.get("data")[0], receipt.receiver_id)
+                        await handle_update_default_human_threshold(parsed_log.get("data")[0], receiver_id)
                     if event_name == "add_or_update_group":
-                            await handle_new_group(parsed_log.get("data")[0], datetime.fromtimestamp(block_timestamp / 1000000000))
+                            await handle_new_group(parsed_log.get("data")[0], now_datetime)
+                    if event_name == "blacklist_account":
+                        await handle_registry_blacklist_action(parsed_log.get("data")[0], receiver_id, now_datetime)
+                    if event_name == "unblacklist_account":
+                        await handle_registry_unblacklist_action(parsed_log.get("data")[0], receiver_id, now_datetime)
                 except json.JSONDecodeError:
                     logger.warning(
                         f"Receipt ID: `{receipt_execution_outcome.receipt.receipt_id}`\nError during parsing logs from JSON string to dict"
@@ -121,17 +128,16 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
             ):
                 if "FunctionCall" not in action:
                     continue
-                receipt = receipt_execution_outcome.receipt
+                # receipt = receipt_execution_outcome.receipt
                 status_obj = receipt_execution_outcome.execution_outcome.outcome
-                created_at = datetime.fromtimestamp(block_timestamp / 1000000000)
 
                 try:
                     function_call = action["FunctionCall"]
                     method_name = function_call["method_name"]
                     args = function_call["args"]
                     decoded_bytes = base64.b64decode(args) if args else b"{}"
-                    signer_id = receipt.receipt["Action"]["signer_id"]
-                    receiver_id = receipt.receiver_id
+                    # signer_id = receipt.receipt["Action"]["signer_id"]
+                    # receiver_id = receiver_id
                     predecessor_id = receipt.predecessor_id
                     result = status_obj.status.get("SuccessValue")
                     # Assuming the decoded data is UTF-8 text
@@ -159,14 +165,14 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
                                     args_dict, receiver_id, signer_id
                                 )
                         case "new":
-                            if match_pot_factory_pattern(receipt.receiver_id):
+                            if match_pot_factory_pattern(receiver_id):
                                 logger.info(f"matched for factory pattern: {args_dict}")
                                 await handle_new_pot_factory(
-                                    args_dict, receiver_id, created_at
+                                    args_dict, receiver_id, now_datetime
                                 )
-                            elif receiver_id.endswith(".nadabot.near"): # does not properly capture registries?
-                                await handle_nadabot_registry(args_dict, receiver_id, created_at)
-                            elif match_pot_subaccount_pattern(receipt.receiver_id):
+                            elif match_nadabot_registry_pattern(".nadabot.near"): # matches registries in the pattern, version(v1).env(staging).nadabot.near
+                                await handle_nadabot_registry(args_dict, receiver_id, now_datetime)
+                            elif match_pot_subaccount_pattern(receiver_id):
                                 logger.info(
                                     f"new pot deployment: {args_dict}, {action}"
                                 )
@@ -176,7 +182,7 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
                                     signer_id,
                                     predecessor_id,
                                     receipt.receipt_id,
-                                    created_at,
+                                    now_datetime,
                                 )
                             break
                         # TODO: update to use handle_apply method??
@@ -190,7 +196,7 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
                                 signer_id,
                                 receipt,
                                 status_obj,
-                                created_at,
+                                now_datetime,
                             )
                             break
 
@@ -204,7 +210,7 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
                                 signer_id,
                                 receipt,
                                 status_obj,
-                                created_at,
+                                now_datetime,
                             )
                             break
 
@@ -324,7 +330,7 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
                                 receiver_id,
                                 signer_id,
                                 receipt.receipt_id,
-                                created_at,
+                                now_datetime,
                             )
                             break
 
@@ -335,14 +341,14 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
                                 receiver_id,
                                 signer_id,
                                 receipt.receipt_id,
-                                created_at,
+                                now_datetime,
                             )
                             break
 
                         case "transfer_payout_callback":
                             logger.info(f"fulfilling payouts..... {args_dict}")
                             await handle_transfer_payout(
-                                args_dict, receiver_id, receipt.receipt_id, created_at
+                                args_dict, receiver_id, receipt.receipt_id, now_datetime
                             )
                             break
 
@@ -374,7 +380,7 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
                             break
                         case "owner_add_admins":
                             logger.info(f"adding admins.. {args_dict}")
-                            await handle_nadabot_admin_add(args_dict, receiver_id)
+                            await handle_add_nadabot_admin(args_dict, receiver_id)
                             break
                         # TODO: handle remove upvote
 
