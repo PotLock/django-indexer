8b6c363c6e2c5fa9d5f7fe6f1c32ceb67a794a5d
Boluwatife Popoola <olaoyejnr@gmail.com>
2024-07-17 17:03:13 +0100

    create util function for common price fetching

--- tokens/models.py
+++ tokens/models.py
@@ -4,6 +4,12 @@ from os import name
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+import requests
+from datetime import timedelta
+
+from base.logging import logger
+from base.utils import format_date
 
 from accounts.models import Account
 
@@ -37,6 +43,12 @@ class Token(models.Model):
         null=False,
         help_text=_("Token decimals."),
     )
+    coingecko_id = models.CharField(
+        _("coingecko_id"),
+        max_length=255,
+        null=True,
+        help_text=_("Token id on coingecko."),
+    )
 
     def get_most_recent_price(self):
         return self.historical_prices.order_by("-timestamp").first()
@@ -45,6 +57,64 @@ class Token(models.Model):
         # Convert the string amount to a Decimal, then adjust by the token's decimal places
         formatted_amount = Decimal(amount_str) / (Decimal("10") ** self.decimals)
         return formatted_amount
+
+    def fetch_usd_prices_common(self, timestamp):
+
+        time_window = timedelta(hours=settings.HISTORICAL_PRICE_QUERY_HOURS or 24)
+        token_prices = TokenHistoricalPrice.objects.filter(
+            token=self,
+            timestamp__gte=timestamp - time_window,
+            timestamp__lte=timestamp + time_window,
+        )
+        existing_token_price = token_prices.first()
+        if existing_token_price:
+            return existing_token_price.price_usd
+
+        price_data = {}
+        if self.coingecko_id:
+            try:
+                logger.info(
+                    "No existing price within acceptable time period; fetching historical pricefrom gecko..."
+                )
+                endpoint = f"{settings.COINGECKO_URL}/coins/{self.coingecko_id}/history?date={format_date(timestamp)}&localization=false"
+                if settings.COINGECKO_API_KEY:
+                    endpoint += f"&x_cg_pro_api_key={settings.COINGECKO_API_KEY}"
+                logger.info(f"coingecko endpoint: {endpoint}")
+                response = requests.get(endpoint)
+                logger.info(f"coingecko response: {response}")
+                if response.status_code == 429:
+                    logger.warning("Coingecko rate limit exceeded")
+                price_data = response.json()
+            except Exception as e:
+                logger.warning(f"Failed to fetch coingecko price data: {e}")
+        price_usd = (
+            price_data.get("market_data", {}).get("current_price", {}).get("usd")
+        )
+        if price_usd:
+            TokenHistoricalPrice.objects.create(
+                token=self,
+                timestamp=timestamp,
+                price_usd=price_usd,
+            )
+            return Decimal(price_usd)
+
+
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            endpoint = f"{settings.COINGECKO_URL}/coins/list?include_platform=true"
+            if settings.COINGECKO_API_KEY:
+                endpoint += f"&x_cg_pro_api_key={settings.COINGECKO_API_KEY}"
+            logger.info(f"coingecko endpoint: {endpoint}")
+            response = requests.get(endpoint)
+            logger.info(f"coingecko response: {response}")
+            if response.status_code == 429:
+                logger.warning("Coingecko rate limit exceeded")
+            price_data = response.json()
+            coin_data = list(filter(lambda x: x["symbol"] == kwargs["symbol"] and x["platforms"].get("near-protocol"), price_data))
+            if coin_data:
+                self.coingecko_id = coin_data[0]["id"]
+        super().save(*args, **kwargs)
 
 
 class TokenHistoricalPrice(models.Model):
