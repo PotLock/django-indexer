5f233b3c37c150ccf169dacb47ea38349a34a86d
Boluwatife Popoola <olaoyejnr@gmail.com>
2024-12-04 14:26:36 +0100

    fix regex for pot factory

--- pots/utils.py
+++ pots/utils.py
@@ -5,18 +5,18 @@ from django.conf import settings
 BASE_PATTERN = (
     r"v\d+\.potfactory\.potlock\.testnet"
     if settings.ENVIRONMENT == "testnet"
-    else (r"staging\.potfactory\.potlock\.near" if settings.ENVIRONMENT == "dev" else r"v\d+\.potfactory\.potlock\.near")
+    else (r"potfactory\.staging\.potlock\.near" if settings.ENVIRONMENT == "dev" else r"v\d+\.potfactory\.potlock\.near")
 )
 
 
 def match_pot_factory_pattern(receiver):
-    """Matches the base pot factory version pattern without a subaccount. NB: does not currently handle testnet factory."""
+    """Matches the base pot factory version pattern without a subaccount."""
     pattern = f"^{BASE_PATTERN}$"
     return bool(re.match(pattern, receiver))
 
 
 def match_pot_subaccount_pattern(receiver):
-    """Matches the pot factory version pattern with a subaccount. NB: does not currently handle testnet factory."""
+    """Matches the pot factory version pattern with a subaccount."""
     pattern = f"^[a-zA-Z0-9_-]+\.{BASE_PATTERN}$"
     return bool(re.match(pattern, receiver))
 
